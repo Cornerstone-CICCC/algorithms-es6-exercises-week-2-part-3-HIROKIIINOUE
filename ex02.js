57d363ddf113a974ece406d6690829f98005d9f5
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-25 20:43:13 -0700

    done ex02.js

--- ex02.js
+++ ex02.js
@@ -1,10 +1,14 @@
 // Exercise 2: Rest Operator
-// Instructions: Write a function named `subtract` that takes any number of arguments and returns their difference. 
-// Use the rest operator to handle the variable number of arguments. 
+// Instructions: Write a function named `subtract` that takes any number of arguments and returns their difference.
+// Use the rest operator to handle the variable number of arguments.
 // Test your function with different numbers of arguments.
 
 function subtract(...numbers) {
-  // Your code here
+  const diff = numbers.reduce((acc, cur) => {
+    return acc - cur;
+  }, numbers[0]);
+
+  return diff;
 }
 
 console.log(subtract(10, 2, 3)); // Example usage
